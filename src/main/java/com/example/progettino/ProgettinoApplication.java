668af220484136b1b49cc3e0c57d09f1e5d4e284
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-11-06 12:37:21 +0100

    first push

---
+++ src/main/java/com/example/progettino/ProgettinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progettino/ProgettinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
